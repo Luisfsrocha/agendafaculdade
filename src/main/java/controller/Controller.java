7b918ede75fe0c615804fb4cba65d6d5f3a35bd2
luisfsrocha <contato.luisr03@gmail.com>
2024-11-10 21:54:37 -0300

    fiz os dados do formulário e encaminhei para a camada MODEL do modelo MVC, armazenando temporariamente estes dados nas variáveis da classe JavaBeans

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,20 +1,20 @@
 package controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private DAO dao = new DAO();
-
+	JavaBeans contato = new JavaBeans();
+	
 	public Controller() {
 		super();
 	}
@@ -26,17 +26,29 @@ public class Controller extends HttpServlet {
 		// Obter a ação solicitada (caminho do servlet)
 		String action = request.getServletPath();
 		System.out.println("Ação solicitada: " + action);
-
 		if (action.equals("/main")) {
-			// Chama o método para listar os contatos
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
 		}
 	}
-
 	// Método para listar os contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		response.sendRedirect("agenda.jsp");
+	}
+
+	// Novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Teste de recebimento
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+
 	}
 }
